8abc4c1d9f022a9177412019498973e24882af62
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-29 23:13:47 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let appName;
+
+</script>
+
+<div class="title-wrap">
+  <h1 class="capitalize-it">{appName}</h1>
+</div>
+
+<style>
+  .title-wrap {
+    display: flex;
+    justify-content: center;
+  }
+  .capitalize-it {
+    text-transform: capitalize;
+  }
+</style>
