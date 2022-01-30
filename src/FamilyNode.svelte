d0649eb77ad0cf3f54425a0f7164cb8137e0ebf7
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-30 15:49:54 +0530

    shared scripts

--- src/FamilyNode.svelte
+++ src/FamilyNode.svelte
@@ -1,8 +1,30 @@
 <script>
   export let member;
+
+  let isActive = false;
+
+  function activate() {
+    if(deactivateNode) {
+      deactivateNode();
+    }
+    isActive = true;
+    deactivateNode = deactivate;
+  }
+
+  function deactivate() {
+    isActive = false;
+  }
+
+  console.log('It will run multiple times.');
 </script>
 
-<div>
+<script context="module">
+  let deactivateNode;
+
+  console.log('It will run one time only.');
+</script>
+
+<div on:click="{activate}" class:active="{isActive}">
   <h1>{member.name}</h1>
   <!-- repeat the component if 'member.isParent' is true -->
   {#if member.isParent}
@@ -15,5 +37,9 @@
 <style>
   div {
     margin-left: 2rem;
+  }
+
+  .active {
+    color: red;
   }
 </style>
